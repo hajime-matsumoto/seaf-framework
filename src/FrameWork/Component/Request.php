b57e883f856db344d149f1409e4ceb446b1ea2ec
Hajime MATSUMOTO <mail@hazime.org>
2014-03-07 00:07:08 +0900

    __constructからinit()を呼び出すように変更

--- src/FrameWork/Component/Request.php
+++ src/FrameWork/Component/Request.php
@@ -38,6 +38,12 @@ class Request
     {
         $this->setUri( $uri );
         $this->setMethod( $method );
+
+        $this->init( );
+    }
+
+    public function init( )
+    {
     }
 
     /**
